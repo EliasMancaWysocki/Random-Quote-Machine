200198ec373e94cc7da4a3d265678b3ea05ba978
Hyperion04 <eliasmanca20@gmail.com>
2022-10-05 22:08:46 -0300

    Creando efecto de colores

--- src/App.js
+++ src/App.js
@@ -1,26 +1,29 @@
 import './App.scss';
+import React, { useEffect, useState } from 'react';
 import { Wrapper } from './components/Wrapper'
 
-var colors = [
-  '#16a085',
-  '#27ae60',
-  '#2c3e50',
-  '#f39c12',
-  '#e74c3c',
-  '#9b59b6',
-  '#FB6964',
-  '#342224',
-  '#472E32',
-  '#BDBB99',
-  '#77B1A9',
-  '#73A857'
-];
-
-let color = colors[Math.floor(Math.random() * colors.length)];
 
 function App() {
+
+  var colors = [
+    '#16a085',
+    '#27ae60',
+    '#2c3e50',
+    '#f39c12',
+    '#e74c3c',
+    '#9b59b6',
+    '#FB6964',
+    '#342224',
+    '#472E32',
+    '#BDBB99',
+    '#77B1A9',
+    '#73A857'
+  ];
+
+  let color = colors[Math.floor(Math.random() * colors.length)];
+
   return (
-    <div className='App' style={{backgroundColor: color, color: color }}>
+    <div className='App' style={{backgroundColor: color, color: color}}>
       <Wrapper 
       color={color} />
     </div>
